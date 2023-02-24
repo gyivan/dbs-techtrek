6af9c20551c851bcbf67479bcffef4fcacef0484
gyivan <gyivan997@gmail.com>
2023-02-24 22:09:24 +0800

    Prepared login page and app template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
+import LoginPage from './components/LoginPage';
+import AppContent from './components/AppContent';
 
 function App() {
+
+  const [error, setError] = useState(null)
+
+  //Login functionality
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleUsernameChange = event => {
+    setUsername(event.target.value)
+  }
+
+  const handlePasswordChange = event => {
+    setPassword(event.target.value)
+  }
+
+  const handleLogin = async event => {
+    event.preventDefault()
+
+    try {
+      const res = await axios.post('http://127.0.0.1:5000/login', {
+        username,
+        password
+        }, {headers: {
+          'Authorization': 'Basic ' + btoa('admin:secret'),
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json'
+        },
+        mode: 'cors'})
+
+        if (res.status === 200) {
+          setIsLoggedIn(true)
+        } else {
+          setError(true)
+        }
+
+    } catch (error) {
+      setError(true)
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      { isLoggedIn ? (
+        // if isLoggedIn is true, display main content
+        <AppContent />
+      ) : (
+        // if isLoggedIn is false, display Login Page
+        <LoginPage 
+        username={username}
+        password={password}
+        error={error}
+        handleUsernameChange={handleUsernameChange}
+        handlePasswordChange={handlePasswordChange}
+        handleLogin={handleLogin} />
+      )
+      }
     </div>
   );
 }
---
+++ src/components/AppContent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AppContent = () => {
+  return (
+    <div className='container'>
+      <h1>INSERT APP CONTENT HERE</h1>
+    </div>
+  )
+}
+
+export default AppContent
