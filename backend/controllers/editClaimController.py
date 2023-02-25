0a9877c3688f3955d4cb7409ca3cc321f935f744
edwincmj <80372170+edwincmj@users.noreply.github.com>
2023-02-25 15:18:09 +0800

    Add get policies by user function

--- backend/controllers/editClaimController.py
+++ backend/controllers/editClaimController.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, jsonify
 from utils.dbConfig import db
-from models.db_models import User,InsuranceClaim
+from models.db_models import User,InsuranceClaim, InsurancePolicy
 from flask_jwt_extended import create_access_token, decode_token
 import json
 import datetime
+from sqlalchemy import inspect
+
+def object_as_dict(obj):
+    return {c.key: getattr(obj, c.key)
+            for c in inspect(obj).mapper.column_attrs}
 
 
 def editClaim():
@@ -35,6 +40,37 @@ def editClaim():
             "code": 201,
             "message":"Successfully Updated"
         }),201
+
+    except Exception as e:
+        print(e)
+        return jsonify(
+            {
+                "code": 500,
+                "message": e
+            }
+        ), 500
+    
+def getPoliciesByUser():
+    try:
+        token = request.json.get("token", None)
+        if not token:
+            return jsonify(
+                {
+                    "code":404,
+                    "message":"No token in request"
+                }
+            )
+        user = decode_token(token, allow_expired=True)
+        print(user['id'])
+        result = db.session.query(InsurancePolicy).join(User, User.EmployeeID==InsurancePolicy.EmployeeID).filter(User.EmployeeID==user['id']).all()
+        data=[]
+        for row in result:
+            data.append(object_as_dict(row))
+        return jsonify({
+            "code": "200",
+            "message": "Successfully retrieved",
+            "result": data
+        }), 200
 
     except Exception as e:
         print(e)
