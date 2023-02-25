0357bdce3ef2ff223daa65f54a559375fc5d6568
JeryllT <100482755+JeryllT@users.noreply.github.com>
2023-02-25 13:24:31 +0800

    remove unessentials of Navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,11 +2,15 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav'
 import { LinkContainer } from 'react-router-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 const NavBar = () => {
+
+    const navigate = useNavigate()
     const handleLogout = (e) => {
       e.preventDefault()
       window.localStorage.removeItem("token")
+      navigate("/login")
     }
 
     return (
@@ -18,9 +22,6 @@ const NavBar = () => {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <LinkContainer to="/view-accounts">
-                <Nav.Link>Manage Insurance Policies</Nav.Link>
-              </LinkContainer>
             </Nav>
             <Nav>
             <LinkContainer to="/" onClick={handleLogout}>
