096c222d1bffe13f13bf13698c0eaf2911be7c9a
JeryllT <100482755+JeryllT@users.noreply.github.com>
2023-02-25 12:28:01 +0800

    updated navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,36 +4,26 @@ import Nav from 'react-bootstrap/Nav'
 import { LinkContainer } from 'react-router-bootstrap'
 
 const NavBar = () => {
+    const handleLogout = (e) => {
+      e.preventDefault()
+      window.localStorage.removeItem("token")
+    }
 
     return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
           <LinkContainer to="/">
-            <Navbar.Brand>SBD Bank</Navbar.Brand>
+            <Navbar.Brand>Insurance Inc.</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               <LinkContainer to="/view-accounts">
-                <Nav.Link>View Accounts</Nav.Link>
+                <Nav.Link>Manage Insurance Policies</Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/make-transactions">
-                <Nav.Link>Make Transactions</Nav.Link>
-              </LinkContainer>
-              {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown> */}
             </Nav>
             <Nav>
-            <LinkContainer to="/" onClick={logout}>
+            <LinkContainer to="/" onClick={handleLogout}>
               <Nav.Link>Log Out</Nav.Link>
             </LinkContainer>
             </Nav>
