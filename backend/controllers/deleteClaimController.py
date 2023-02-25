5b6e04be19839f3794ca44460a7a26b32bbaffcc
gyivan <gyivan997@gmail.com>
2023-02-25 15:49:08 +0800

    Updated deleteClaim

---
+++ backend/controllers/deleteClaimController.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from utils.dbConfig import db
+from models.db_models import InsuranceClaim
+from sqlalchemy.orm import joinedload
+from flask_jwt_extended import jwt_required
+ 
+@jwt_required()
+def deleteClaimById(claimID):
+
+    try:
+        #delete claim
+        InsuranceClaim.query.filter(InsuranceClaim.ClaimID == claimID).delete()
+
+        db.session.commit()
+
+        return jsonify({
+            "data": f"Claim of id {claimID} has been successfully deleted"
+        })
+    except:
+        return jsonify({
+            "data": "Server error"
+        }), 500
---
+++ backend/routes/deleteClaimRoute.py
@@ -0,0 +1,9 @@
+from flask import Blueprint; 
+
+from controllers.deleteClaimController import deleteClaimById; 
+
+
+deleteClaimRoute = Blueprint('delete_claim', __name__)
+
+
+deleteClaimRoute.route('/deleteClaim/<int:claimID>', strict_slashes=False, methods=['DELETE'])(deleteClaimById)
